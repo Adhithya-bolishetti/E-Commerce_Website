62c14f6451fa92669fbe042a999743dfbca2c759
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-07-15 00:05:43 +0530

    feat: Add Breadcrum component and integrate routing in Item component

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useContext } from 'react';
 import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom';
 
 export const Product = () => {
   const {all_product} = useContext(ShopContext);
-
+  const {productId} = useParams();
+  const product = all_product.find((e)=>e.id === Number(productId))
   return (
-    <div>Product</div>
+    <div>
+      {/* <Breadcrum product={product}/> */}
+    </div>
   )
 }
